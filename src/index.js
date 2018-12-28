e9d015d232c8435b073f59f58ea8af554ea96b49
Janice novacek <janicenovacek@Janices-MacBook-Pro.local>
2018-12-27 19:22:25 -0500

    set up files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    const App = () => {
+        return (
+            <div>
+                <Message />
+            </div>
+        );
+    }
+    
+    const Message = (props) => {
+        return (
+            <div className="ui message">
+                <div className="header">Changes in Service</div>
+                <p>We just updated our privacy policy here to better service our customers.</p>
+            </div>
+        );
+    }
+    
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // Renders the App component into a div with id 'root'
+    ReactDOM.render(<App />, document.querySelector('#root'));
+
+
+
+
+
+
+
